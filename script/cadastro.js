1974a341bbaebae4fb2da4531fbd10831da2bab0
carlosrmfernandes <carlosr.m.fernandes@hotmail.com>
2024-04-11 19:39:28 -0300

    Update cadastro.js

--- script/cadastro.js
+++ script/cadastro.js
@@ -1,4 +1,4 @@
-const url = 'https://api-go-wash-efc9c9582687.herokuapp.com/api/user';
+const url = 'https://go-wash-api.onrender.com/api/user';
 
 async function cadastroUsuario(){   
     var name = document.getElementById('name');     
