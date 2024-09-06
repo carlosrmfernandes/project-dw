104a16aaec4c3fceb41e3495bf7f34a19534a7b4
Carlos Rafael Magalhães Fernandes <Carlos.Fernandes@salas.aulas>
2024-09-05 22:16:31 -0300

    ok

--- script/cadastro.js
+++ script/cadastro.js
@@ -1,32 +1,29 @@
-const url = 'https://go-wash-api.onrender.com/api/user';
 
-async function cadastroUsuario(){   
-    var name = document.getElementById('name');     
-    let resposta = await fetch(url,{
+const url = "https://go-wash-api.onrender.com/api/user"; 
+async function cadastroUsuario(){
+    let name = document.getElementById('name').value
+    let email = document.getElementById('email').value
+    let api = await fetch(url,{
         method:"POST",
-        body:JSON.stringify(
-            {
-                "name":name.value,
-                "email":"carlos@gmail.com",
-                "user_type_id":1,
-                "password": "123456",
-                "cpf_cnpj": "62418247406",
-                "terms": 1,
-                "birthday":"2000-10-12"    
-            }
-        ),
+        body:JSON.stringify({
+            "name":name,
+            "email":email,
+            "user_type_id":1,
+            "password": "123456",
+            "cpf_cnpj": "00188338020",
+            "terms": 1,
+            "birthday":"2000-10-12"    
+        }),
         headers:{
-            'Content-Type': 'application/json'
-        }        
+            'Content-Type':'application/json'
+        }
     });
 
-    let data = await resposta.json();
-
-
-    if(data.data.statusCode != 200){
-        alert(data.data.errors?.cpf_cnpj[0]);
-        return;
+    if(api.ok){
+        let resposta = await api.json();
+        console.log(resposta)
+        return
     }
-    alert("Cadastro feito com sucesso");
-    window.location.href = "login.html";
+    let respostaErro = await api.json();
+        console.log(respostaErro.data.errors.cpf_cnpj)
 }
